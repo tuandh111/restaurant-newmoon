840f27965a2ddc133ee7e7122ff2ff0e1a8c7311
Hoàng Tuấn <129180520+tuandh111@users.noreply.github.com>
2025-06-15 18:15:09 +0700

    update

--- controller/Calandar-controller.js
+++ controller/Calandar-controller.js
@@ -1,3 +1,18 @@
-app.controller('CalendarController', function ($scope, $http, $window, API_BASE_URL) {
+app.controller('CalendarController', function ($scope, $http, $window, API_BASE_URL,$timeout) {
     // Toastify helper
+        $timeout(function () {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    height: 'auto'
+    // ❌ Không cần plugins nếu bạn dùng bản bundle (main.min.js)
+  });
+  calendar.render();
+}, 0);
+
 });
